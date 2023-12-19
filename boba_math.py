f9b464572f343cfcec78cd6a88ed80c267630d00
Alex Kumar <alexkumar520@gmail.com>
2023-12-19 11:53:28 -0800

    Fixing parse of dollar by using regex.

--- boba_math.py
+++ boba_math.py
@@ -1,14 +1,23 @@
+import re
+
+pattern = r'^\$?\d+(,\d{3})*(\.[0-9]{2})?$'
+
 from math import ceil
 def boba_calc(amount: str):
-    try:
-        converted_amount = amount.replace("$", "").replace(",", "")
-        int_amt = int(float(converted_amount)) # checks if decimal is in string and should resolve. 
-        conversion = ceil(int_amt / 6.50)
+    
+    if not re.match(pattern, amount):
+        return "Please use a proper price: " + amount
+    
+    converted_amount = amount.replace("$", "").replace(",", "")
+    amt = float(converted_amount) # checks if decimal is in string and should resolve.
+    if amt < 6.50: 
+        return "It's basically free! Why are you bothering with this command?" 
+    
+    conversion = ceil(int(amt) / 6.50)
 
-        if "$" not in amount:
-            amount = "$" + amount
+    if "$" not in amount:
+        amount = "$" + amount
 
-        return "When you pay {0}, that's about {1} bobas! Isn't that concerning?".format(amount, conversion)
-    except:
-        return "Please use a proper dollar amount to calculate bobas."
+    return "When you pay {0}, that's about {1} bobas! Isn't that concerning?".format(amount, conversion)
+    
     
