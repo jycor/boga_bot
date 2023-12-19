6b9bc3a2852bdd21bd66d90b1e3caacce4aeea57
Alex Kumar <43835304+alexkumar520@users.noreply.github.com>
2023-12-19 00:02:54 -0800

    Fixing int division. (#6)

--- boba_math.py
+++ boba_math.py
@@ -2,7 +2,7 @@ from math import ceil
 def boba_calc(amount: str):
     try: 
         int_amt = int(amount)
-        conversion = ceil(int_amt // 6.50)
+        conversion = ceil(int_amt / 6.50)
         return "That'd cost you about {0} bobas.".format(conversion)
     except:
         return ("Incorrect amount sent, please fix : o")
