4b3e7f386c232809c5409c0a9ef0268e36ae5499
Alex Kumar <43835304+alexkumar520@users.noreply.github.com>
2023-12-19 11:54:49 -0800

    Boba currency handler (#9)

--- boba_math.py
+++ boba_math.py
@@ -1,8 +1,23 @@
+import re
+
+pattern = r'^\$?\d+(,\d{3})*(\.[0-9]{2})?$'
+
 from math import ceil
 def boba_calc(amount: str):
-    try: 
-        int_amt = int(amount)
-        conversion = ceil(int_amt / 6.50)
-        return "That'd cost you about {0} bobas.".format(conversion)
-    except:
-        return ("Incorrect amount sent, please fix : o")
+    
+    if not re.match(pattern, amount):
+        return "Please use a proper price: " + amount
+    
+    converted_amount = amount.replace("$", "").replace(",", "")
+    amt = float(converted_amount) # checks if decimal is in string and should resolve.
+    if amt < 6.50: 
+        return "It's basically free! Why are you bothering with this command?" 
+    
+    conversion = ceil(int(amt) / 6.50)
+
+    if "$" not in amount:
+        amount = "$" + amount
+
+    return "When you pay {0}, that's about {1} bobas! Isn't that concerning?".format(amount, conversion)
+    
+    
