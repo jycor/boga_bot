31ad64664a50411d8c25ece5700cef902b9300a1
Alex Kumar <alexkumar520@gmail.com>
2024-03-17 11:30:31 -0700

    Fixing daily announcement to exclude GIF url. Changed timezone usage back to original method by using Pacific.

--- daily_task.py
+++ daily_task.py
@@ -1,5 +1,5 @@
 from discord.ext import tasks
-
+from zoneinfo import ZoneInfo
 from datetime import datetime, timezone, timedelta, time
 
 import consts
@@ -7,12 +7,16 @@ import youtube
 import urban_dict
 import gifgenerate
 
-pst = timezone(timedelta(hours=-8))
+pst = ZoneInfo(key='America/Los_Angeles')
 daily_msg_time = time(hour=8, tzinfo=pst)
+
 
 # USE THIS FOR TESTING
 # dt = datetime.now() + timedelta(seconds=10)
 # daily_msg_time = time(hour=dt.hour, minute=dt.minute, second=dt.second, tzinfo=pst)
+
+# ANOTHER DEBUG METHOD IN CASE ABOVE NOT WORKING: 
+# @tasks.loop(seconds=5.0)
 
 @tasks.loop(time=daily_msg_time)
 async def send_daily_msg(bot):
@@ -23,7 +27,8 @@ async def send_daily_msg(bot):
   greeting = "Good morning everyone!"
   daily_word_msg = "The Word of the Day is:\n{0}".format(urban_dict.word_of_the_day())
   daily_yt_vid = "The #1 trending video on YouTube is:\n{0}".format(youtube.get_trending())
-  daily_gif = "The Gif of the Day is:\n{0}".format(gifgenerate.generate_gif())
-  msg = "{0}\n{1}\n{2}\n{3}".format(greeting, daily_word_msg, daily_yt_vid, daily_gif)
+  msg = "{0}\n{1}\n{2}".format(greeting, daily_word_msg, daily_yt_vid)
+  gif_url = gifgenerate.generate_gif()
 
   await ctx.send(msg)
+  await ctx.send(gif_url)
