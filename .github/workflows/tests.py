5cd373f70534c078b43393ed4bd59fabe3d773c7
Alex Kumar <alexkumar520@gmail.com>
2025-01-17 19:44:27 -0800

    Added test to see if env variables are working.

--- .github/workflows/tests.py
+++ .github/workflows/tests.py
@@ -1,7 +1,10 @@
+import boba_math
+import gifgenerate
 import pytest
 
-def func(x):
-    return x + 1
 
-def test_answer():
-    assert func(3) == 4
+def test_boba():
+    assert boba_math.calc("6") == "It's basically free! Why are you bothering with this command?"
+
+def test_gif_generate():
+    assert gifgenerate.generate_gif() != None
